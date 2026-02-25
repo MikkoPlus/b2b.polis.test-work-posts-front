fc6a4c6ce7043cc869e58a775d86a4ca25946aa9
mikkoplus <m1kkoplusodin@gmail.com>
2026-02-25 19:59:32 +0300

    add header, styling homeage, fix padding

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,13 +1,26 @@
 import { Link } from "react-router-dom";
+import styles from "./HomePage.module.scss";
 
 export const HomePage = () => {
   return (
     <div className="wrapper">
-      <h1>Блог о фронтенде</h1>
-      <p>Добро пожаловать! Здесь собраны статьи о React, TypeScript и современной веб-разработке.</p>
-      <Link to="/articles" className="cta-link">
-        Перейти к статьям
-      </Link>
+      <section className={styles.home}>
+        <h1 className={styles.home__title}>Блог о фронтенде</h1>
+        <p className={styles.home__subtitle}>
+          Добро пожаловать! Здесь собраны статьи о React, TypeScript и современной веб‑разработке.
+        </p>
+        <div className={styles.home__actions}>
+          <Link to="/articles" className={styles.home__link}>
+            Список статей
+          </Link>
+          <Link
+            to="/newArticle"
+            className={`${styles.home__link} ${styles.home__link_primary}`}
+          >
+            Создать статью
+          </Link>
+        </div>
+      </section>
     </div>
   );
 };
